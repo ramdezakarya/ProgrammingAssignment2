7062621455a95a558fc7771ab199c6860ef2eeb6
zakaya <ramdezakarya88@yahoo.fr>
2015-02-19 17:53:17 +0000

    commit for solving

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Functions explanations 
-## Write a short comment describing this function
+## This first function makeCacheMatrix caches 
+## a matrix that is an object in R.
+## Let's us remark that, to be solve, the matrix should 
+## be inversible i.e means there is square matrix and det (matrix) different to 0.
+## In R, we use solve function as recommended. Also, we assume for
+## this assignment that, the matrix supplied is always invertible.
+
+## This functions will help to resolve this specified problem
+## makeCacheMatrix is an object that stores a matrix by
+## set values of a matrix, get its values and set the inverse
+## of a matrix and get the inverse values.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL 
